c3d897f7ce264b99a2055bc74c1f3eef230f51e1
Paul Sobocinski <psobocinski@connected.io>
2019-07-09 17:02:19 -0400

    Print common metrics once; print simulation type

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 
 const { arrivalProbability, durationInTicks, timeOnRoadFunction } = require('./config'),
       ArrivalSequence = require('./lib/ArrivalSequence'),
-      Simulation = require('./lib/Simulation');
+      Simulation = require('./lib/Simulation'),
+      MetricsCalculator = require('./lib/MetricsCalculator');
 
 const arrivalSequence = (new ArrivalSequence(durationInTicks, arrivalProbability)).generate(),
       simulateRandom = new Simulation('random', timeOnRoadFunction, arrivalSequence);
+
+MetricsCalculator.printCommonMetrics(arrivalSequence);
 
 simulateRandom.run();
