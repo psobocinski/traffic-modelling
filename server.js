3cf1f7913810e0bd2f60ae470d1f025ec11301e2
Paul Sobocinski <psobocinski@connected.io>
2019-07-12 00:17:04 -0400

    Support multiple simulation iterations

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ let arrivalSequence, config, simulation, simulations = [];
 arrivalSequence = (new ArrivalSequence(commonConfig)).generate();
 
 simulationsConfig.forEach(simulationConfig => {
-  config = Object.assign(commonConfig, simulationConfig);
+  config = Object.assign({}, commonConfig, simulationConfig);
   simulation = new Simulation(config, arrivalSequence);
   simulations.push(simulation);
 });
 
 simulations[0].metricsCalculator.printCommonMetrics();
-simulations.forEach(simulation => simulation.run());
+
+for (let i = 1; i <= commonConfig.numberOfIterations; i++) {
+  console.log(`\n\nITERATION ${i}`);
+
+  simulations.forEach(simulation =>
+    simulation.run());
+}
