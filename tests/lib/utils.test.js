c1edca2c3ef8ed6d70fe776d661b474e7e6b5b29
Paul Sobocinski <psobocinski@connected.io>
2019-07-11 23:19:37 -0400

    Remove unused files; add tests

---
+++ tests/lib/utils.test.js
@@ -0,0 +1,42 @@
+const utils = require('../../lib//utils');
+
+describe.only('utils', () => {
+  describe('newArray2D', () => {
+    let newArray;
+
+    it('creates an empty 2D array of given length', () => {
+      newArray = utils.newArray2D(2);
+
+      expect(newArray).toEqual([[], []]);
+    });
+
+    // test to ensure (new Array(arrayLength)).fill([]) is not used (as it creates empty arrays by reference)
+    it('creates empty arrays by value (i.e. distinct copies)', () => {
+      newArray = utils.newArray2D(2);
+
+      newArray[1].push('x');
+
+      expect(newArray).toEqual([[], ['x']]);
+    });
+  });
+
+  describe('indexOfShortestLane', () => {
+    let lanes, c;
+
+    it('returns the index of shortest lane', () => {
+      lanes = [ [c], [] ];
+
+      index = utils.indexOfShortestLane(lanes);
+
+      expect(index).toEqual(1);
+    });
+
+    it('returns the lowest index when lanes have same length', () => {
+      lanes = [ [c, c], [c, c] ];
+
+      index = utils.indexOfShortestLane(lanes);
+
+      expect(index).toEqual(0);
+    });
+  });
+});
