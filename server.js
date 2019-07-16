426e750464e03512af45d2faf44c781fa8af59c3
Paul Sobocinski <psobocinski@connected.io>
2019-07-16 17:01:17 -0400

    Take average of simulation output and write to file

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 'use strict';
 
-const commonConfig = require('./config/common'),
-      simulationsConfig = require('./config/simulations'),
-      ArrivalSequence = require('./lib/ArrivalSequence'),
-      MetricsCalculator = require('./lib/MetricsCalculator'),
-      Simulation = require('./lib/Simulation');
+const fs = require('fs'),
+  commonConfig = require('./config/common'),
+  simulationsConfig = require('./config/simulations'),
+  ArrivalSequence = require('./lib/ArrivalSequence'),
+  Simulation = require('./lib/Simulation');
 
-let arrivalSequence, config, simulation, simulations = [];
+let arrivalSequence, config, simulation, simulations = [], simulationResults, table;
+
+const rowHeaderLabels = [
+  'Min Time in Lane',
+  'Time Saved %',
+  'Avg Time',
+  'Cars Remain',
+  'Aggregate Time',
+  'Aggregate Throughput (cars / 100 ticks)',
+];
+
+const rowHeaderFormatters = {
+  csv: row => row.join(','),
+  markdown: row => `| ${row.join(' | ')} |\n| ${new Array(row.length).fill('---').join(' | ')} |`
+};
+
+
+const rowFormatters = {
+  csv: row => row.join(','),
+  markdown: row => `| ${row.join(' | ')} |`
+};
+
+function generateTable(results, format) {
+  const rowHeaderFormatter = rowHeaderFormatters[format],
+    rowFormatter = rowFormatters[format];
+  let rows, rowHeaders;
+
+  rows = results.map(result => rowFormatter(result));
+  rowHeaders = [rowHeaderFormatter(rowHeaderLabels)];
+
+  return rowHeaders.concat(rows).join('\n');
+}
+
+function createOutputFile({ outputPath, outputFormat }, results) {
+  const fileName = `results.${outputFormat}`,
+    pathAndFileName = `${outputPath}/${fileName}`;
+
+  fs.writeFile(pathAndFileName, `${results}\n`, function(err) {
+    if(err) return console.log(err);
+
+    console.log(`results outputted to file: ${pathAndFileName}`);
+  });
+}
 
 arrivalSequence = (new ArrivalSequence(commonConfig)).generate();
 
@@ -18,11 +60,9 @@ simulationsConfig.forEach(simulationConfig => {
 
 simulations[0].metricsCalculator.printCommonMetrics();
 
-for (let i = 1; i <= commonConfig.numberOfIterations; i++) {
-  console.log(`\n\nITERATION ${i}`);
+simulationResults = simulations.map(simulation =>
+  simulation.run());
 
-  MetricsCalculator.printTableHeaders(commonConfig.outputFormat);
+table = generateTable(simulationResults, commonConfig.outputFormat);
 
-  simulations.forEach(simulation =>
-    simulation.run());
-}
+createOutputFile(commonConfig, table);
