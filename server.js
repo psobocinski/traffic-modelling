9f4e6af0b802cd26634243b99533f4860e2b7e1f
Paul Sobocinski <psobocinski@connected.io>
2019-07-18 21:24:09 -0400

    Refactor

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const commonConfig = require('./config/common'),
   simulationsConfig = require('./config/simulations'),
   SimulationRunner = require('./lib/SimulationRunner'),
-  { TrafficModel } = require('./lib/TrafficModel');
+  TrafficModel = require('./lib/traffic-model/TrafficModel');
 
 const trafficModel = new TrafficModel(commonConfig),
   runner = new SimulationRunner(commonConfig, simulationsConfig, trafficModel);
