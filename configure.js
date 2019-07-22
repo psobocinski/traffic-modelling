33708b23e2fd5893e07bf47ac74a194bfeef1356
Paul Sobocinski <psobocinski@connected.io>
2019-07-22 11:59:12 -0400

    Add input validation

--- configure.js
+++ configure.js
@@ -1,50 +1,80 @@
 const fs = require('fs'),
   inquirer = require('inquirer'),
   independentVariables = [
-    'laneChangeDelayTicks',
-    'minTicksInLane',
-    'minTickSpeedupPercent',
-    'numberOfLanes'
+    { name: 'laneChangeDelayTicks', type: 'number' },
+    { name: 'minTicksInLane', type: 'integer', minimum: 0 },
+    { name: 'minTickSpeedupPercent', type: 'number' },
+    { name: 'numberOfLanes', type: 'integer', minimum: 2 }
   ],
+  validators = {
+    integer: input => Number.isInteger(parseFloat(input)) ? true : 'Must be an integer',
+    number: input => Number.isNaN(parseFloat(input)) ? 'Must be a number' : true
+  },
   outputPathAndFilename = './config/simulations.json';
 
-let simulations = [];
+function validateSmallestValue(input, answers) {
+  const selectedIndependentVariable = independentVariables.find(({ name }) =>
+    name === answers.independentVariable);
+
+  if (parseFloat(input) < selectedIndependentVariable.minimum)
+    return `Must be greater than or equal to ${selectedIndependentVariable.minimum}`;
+
+  return validators[selectedIndependentVariable.type](input);
+}
+
+function validateIncrement(input, answers) {
+  const selectedIndependentVariable = independentVariables.find(({ name }) =>
+    name === answers.independentVariable);
+
+  if (parseFloat(input) <= 0)
+    return 'Increment must be greater than 0';
+
+  return validators[selectedIndependentVariable.type](input);
+}
+
+function validateNumberOfSimulations(input) {
+  if (parseFloat(input) <= 0)
+    return 'Number of simulations must be greater than 0';
+
+  return validators.integer(input);
+}
 
 inquirer
   .prompt([
     {
       type: 'list',
       name: 'independentVariable',
-      message: 'Select the independent variable:',
-      choices: independentVariables
+      message: 'Select the independent variable',
+      choices: independentVariables.map(({ name }) => name)
     },
     {
-      type: 'number',
       name: 'smallestValue',
-      message: 'Set variable\'s smallest value'
+      message: 'Set variable\'s smallest value',
+      validate: validateSmallestValue
     },
     {
-      type: 'number',
       name: 'increment',
-      message: 'Set variable\'s increment'
+      message: 'Set variable\'s increment',
+      validate: validateIncrement
     },
     {
-      type: 'number',
       name: 'numberOfSimulationRuns',
-      message: 'Set number of simulation runs'
+      message: 'Set number of simulation runs',
+      validate: validateNumberOfSimulations
     },
   ])
   .then(({ independentVariable, smallestValue, increment, numberOfSimulationRuns }) => {
-    let simulation;
+    let simulation, simulations = [];
 
-    for (let value = smallestValue; simulations.length < numberOfSimulationRuns; value += increment) {
+    for (let value = parseFloat(smallestValue); simulations.length < parseInt(numberOfSimulationRuns); value += parseFloat(increment)) {
       simulation = {};
       simulation[independentVariable] = value;
       simulations.push(simulation);
     }
 
-    fs.writeFile(outputPathAndFilename, JSON.stringify(simulations, null, 2), function(err) {
-      if(err) return console.log(err);
+    fs.writeFile(outputPathAndFilename, JSON.stringify(simulations, null, 2), function(error) {
+      if (error)
+        return console.log(error);
 
       console.log(`results outputted to file: ${outputPathAndFilename}`);
     });
