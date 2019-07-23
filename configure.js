93f317b8cbabc39d776d85748c39e33ceff3dca4
Paul Sobocinski <psobocinski@connected.io>
2019-07-23 13:21:51 -0400

    Refactor

--- configure.js
+++ configure.js
@@ -1,61 +1,8 @@
 'use strict';
 
-const { createConfigFile } = require('./lib/utils'),
-  inquirer = require('inquirer'),
-  independentVariables = [
-    { name: 'laneChangeDelayTicks', type: 'number' },
-    { name: 'minTicksInLane', type: 'integer', minimum: 0 },
-    { name: 'minTickSpeedupPercent', type: 'number' },
-    { name: 'numberOfLanes', type: 'integer', minimum: 2 }
-  ],
-  ConfigValidator = require('./lib/ConfigValidator');
+const independentVariables = require('./lib/traffic-model/independentVariables'),
+  SimulationsConfigurator = require('./lib/SimulationsConfigurator');
 
-function buildPrompts(independentVariables, validator) {
-  return [
-    {
-      type: 'list',
-      name: 'independentVariable',
-      message: 'Select the independent variable',
-      choices: independentVariables.map(({name}) => name)
-    },
-    {
-      name: 'smallestValue',
-      message: 'Set variable\'s smallest value',
-      validate: validator.validateSmallestValue.bind(validator)
-    },
-    {
-      name: 'increment',
-      message: 'Set variable\'s increment',
-      validate: validator.validateIncrement.bind(validator)
-    },
-    {
-      name: 'numberOfSimulationRuns',
-      message: 'Set number of simulation runs',
-      validate: validator.validateNumberOfSimulations
-    },
-  ];
-}
+const configurator = new SimulationsConfigurator(independentVariables);
 
-function processAnswers(configFileName) {
-  return ({independentVariable, smallestValue, increment, numberOfSimulationRuns}) => {
-    let simulation, simulations = [];
-
-    for (
-      let value = parseFloat(smallestValue);
-      simulations.length < parseInt(numberOfSimulationRuns);
-      value += parseFloat(increment)
-    ) {
-      simulation = {};
-      simulation[independentVariable] = value;
-      simulations.push(simulation);
-    }
-
-    createConfigFile(configFileName, simulations);
-  };
-}
-
-const validator = new ConfigValidator(independentVariables);
-
-inquirer
-  .prompt(buildPrompts(independentVariables, validator))
-  .then(processAnswers('simulations.json'));
+configurator.configure();
