71eb55dcee2feba2e9ffc597274c116b1c5b331c
Paul Sobocinski <psobocinski@connected.io>
2019-07-23 21:08:56 -0400

    npm run configure generates common config as well

--- configure.js
+++ configure.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const independentVariables = require('./lib/traffic-model/independentVariables'),
+  CommonConfigInitializer = require('./lib/CommonConfigInitializer'),
   SimulationsConfigurator = require('./lib/SimulationsConfigurator');
 
 const configurator = new SimulationsConfigurator(independentVariables);
 
+CommonConfigInitializer.initialize();
 configurator.configure();
